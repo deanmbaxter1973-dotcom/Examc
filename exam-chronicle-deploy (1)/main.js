c3e2ba11b2f3a7be419c0977b6f8d0f15cbbe9c3
deanmbaxter1973-dotcom <deanmbaxter1973@yahoo.com>
2026-07-09 11:05:26 +0100

    Add files via upload

---
+++ exam-chronicle-deploy (1)/main.js
@@ -0,0 +1,4 @@
+import{createRoot}from"https://esm.sh/react-dom@18.3.1/client";
+import React from"https://esm.sh/react@18.3.1";
+import App from"./app.js";
+createRoot(document.getElementById("root")).render(React.createElement(React.StrictMode,null,React.createElement(App)));
